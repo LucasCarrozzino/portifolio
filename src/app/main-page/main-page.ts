62b4f7be951df4c515f8d663e5bbb093de61b036
LucasCarrozzino <lucass.crz@gmail.com>
2025-09-29 17:30:46 -0300

    rewrited all components content

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './main-page.scss'
 })
 export class MainPage {
-
+  constructor(private _router:Router){}
+  
+  public onClickExplore(){
+    this._router.navigate(["/about"]);
+  }
 }
